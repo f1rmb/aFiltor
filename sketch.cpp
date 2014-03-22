dbb94b7b6c5ede21a32e2ece6115d9af592bf127
Daniel Caujolle-Bert <danielvador@gmail.com>
2014-03-22 20:56:27 +0100

    Improve memory usage.

--- sketch.cpp
+++ sketch.cpp
@@ -25,20 +25,19 @@
 
 #include "MemoryFree.h"
 
-static const String         PROGRAM         = "aFiltor";
-static const uint8_t        VERSION_MAJOR   = 0;
-static const uint8_t        VERSION_MINOR   = 2;
-static const uint8_t        VERSION_SUB     = 0;
+static const uint8_t        VERSION_MAJOR           = 0;
+static const uint8_t        VERSION_MINOR           = 2;
+static const uint8_t        VERSION_SUB             = 0;
 
-static const uint8_t        GLYPH_MARKER    = 0;
-static const uint8_t        GLYPH_FOCUS     = 1;
-static const uint8_t        GLYPH_LIGHT     = 2;
-static const uint8_t        GLYPH_SPACE     = 32;
-static const uint8_t        GLYPH_MINUS     = 45;
+static const uint8_t        GLYPH_MARKER            = 0;
+static const uint8_t        GLYPH_FOCUS             = 1;
+static const uint8_t        GLYPH_LIGHT             = 2;
+static const uint8_t        GLYPH_SPACE             = 32;
+static const uint8_t        GLYPH_MINUS             = 45;
 
-static const unsigned long  BCL_TIMEOUT     = 15000;
+static const unsigned long  BCL_TIMEOUT             = 15000;
 
-static const uint8_t        _glyphs[3][8]   =
+static const uint8_t        _glyphs[3][8] PROGMEM   =
 {
     {
         B11111,
@@ -109,12 +108,12 @@ void displayAtt()
 
     mKeypad.write(a > 0 ? GLYPH_MINUS : GLYPH_SPACE);
     mKeypad.print(a, DEC);
-    mKeypad.print(" dB");
+    mKeypad.print(F(" dB"));
 
     mKeypad.setCursor(3, 1);
 
     if (!mAttFocused)
-        mKeypad.print("  ");
+        mKeypad.print(F("  "));
     else
     {
         if (mAttUnity)
@@ -175,14 +174,14 @@ void displayUpdate()
 //
 void displayBanner(unsigned long timeout = 1500)
 {
-    char buf[17];
+    char buf[mKeypad.getCols() + 1];
 
-    snprintf(buf, sizeof(buf), "%s %u.%u.%u", PROGRAM.c_str(), VERSION_MAJOR, VERSION_MINOR, VERSION_SUB);
+    snprintf(buf, sizeof(buf), "aFiltor %u.%u.%u", VERSION_MAJOR, VERSION_MINOR, VERSION_SUB);
 
     mKeypad.setCursor(0, 0);
     mKeypad.printCenter(buf);
     mKeypad.setCursor(0, 1);
-    mKeypad.printCenter("f1rmb - 2014");
+    mKeypad.printCenter(F("f1rmb - 2014"));
     delay(timeout);
     mKeypad.clear();
 }
@@ -199,7 +198,7 @@ void displayInfo()
 
     mKeypad.clear();
     mKeypad.setCursor(0, 0);
-    mKeypad.print("Free Memory:");
+    mKeypad.print(F("Free Memory:"));
     mKeypad.setCursor(mKeypad.getCols() - strlen(buf), 1);
     mKeypad.print(buf);
     delay(2500);
@@ -215,13 +214,20 @@ void setup()
 {
     mKeypad.setBacklightTimeout(BCL_TIMEOUT);
 
-    mFilters.SetUserFilterName(dhwFilters::FILTER_USER_1, " Bypass ");
+    mFilters.SetUserFilterName(dhwFilters::FILTER_USER_1, F(" Bypass "));
     mFilters.SetUserFilterEnabled(dhwFilters::FILTER_USER_1);
 
     mFilterMaxLen = mFilters.GetMaxNameLength();
 
     for (uint8_t i = 0; i < sizeof(_glyphs) / sizeof(_glyphs[0]); i++)
-        mKeypad.createChar(i, (uint8_t *)_glyphs[i]);
+    {
+        uint8_t gl[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        for (uint8_t j = 0 ; j < 8; j++)
+            gl[j] = pgm_read_byte(&_glyphs[i][j]);
+
+        mKeypad.createChar(i, gl);
+    }
 
     displayBanner();
     displayUpdate();
@@ -300,9 +306,8 @@ void loop()
                     if (mAtt.GetValue() >= 10) // Switch from unity and tenth, if attenuation value is >= 10
                         mAttUnity = !mAttUnity;
                     else
-                    {
                         mAtt.Inc(10); // Quickly increment +10 if attenuation is < 10
-                    }
+
                     displayAtt();
                 }
                 else
